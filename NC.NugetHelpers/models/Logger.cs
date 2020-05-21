23660076ec66652b79a8916b5deda305ecff224a
Nathaniel Collier <drxs3v3n@gmail.com>
2020-05-21 13:58:21 -0500

    More work towards getting package search to work

---
+++ NC.NugetHelpers/models/Logger.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using NuGet.Common;
+
+namespace NC.NugetHelpers.models
+{
+    public class Logger : NuGet.Common.ILogger
+    {
+        public void LogDebug(string data) => System.Diagnostics.Debug.WriteLine( $"DEBUG: {data}");
+        public void LogVerbose(string data) => System.Diagnostics.Debug.WriteLine( $"VERBOSE: {data}");
+        public void LogInformation(string data) => System.Diagnostics.Debug.WriteLine( $"INFORMATION: {data}");
+        public void LogMinimal(string data) => System.Diagnostics.Debug.WriteLine( $"MINIMAL: {data}");
+        public void LogWarning(string data) => System.Diagnostics.Debug.WriteLine( $"WARNING: {data}");
+        public void LogError(string data) => System.Diagnostics.Debug.WriteLine( $"ERROR: {data}");
+        public void LogSummary(string data) => System.Diagnostics.Debug.WriteLine( $"SUMMARY: {data}");
+    }
+}
