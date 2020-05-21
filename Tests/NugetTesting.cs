785bd802aa1473fd6603b11b96646ffa11f74433
Nathaniel Collier <drxs3v3n@gmail.com>
2020-05-21 13:46:41 -0500

    Working through code needed to search for packages

--- Tests/NugetTesting.cs
+++ Tests/NugetTesting.cs
@@ -14,5 +14,21 @@ namespace dotnetTestsGeneral
 
             Assert.IsTrue(sources.Count() > 0);
         }
+
+
+        [TestMethod]
+        public async Task SearchForAPackage(){
+            var topNuget = NC.NugetHelpers.Utility.GetPackageSources().First();
+
+            var source = new NuGet.Protocol.Core.Types.SourceRepository(source: topNuget, 
+                                providers: NuGet.Protocol.Core.Types.Repository.Provider.GetCoreV3()
+                        );
+            var packageSearchResource = await source.GetResourceAsync<NuGet.Protocol.Core.Types.PackageSearchResource>();
+            var resullts = await packageSearchResource.SearchAsync(searchTerm: "newtonsoft",
+                                        filters: new NuGet.Protocol.Core.Types.SearchFilter(),
+                                        skip: 0,
+                                        take: 5,
+                                        log: )
+        }
     }
 }
